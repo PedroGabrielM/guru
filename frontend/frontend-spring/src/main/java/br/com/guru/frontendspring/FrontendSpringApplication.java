e97552e6950e578e5f62f2de8dba8f6ec7986c5d
PedroGabrielM <pedrombalduino@gmail.com>
2023-08-13 17:10:49 -0300

    First commit, create modules and create layout frontend

---
+++ frontend/frontend-spring/src/main/java/br/com/guru/frontendspring/FrontendSpringApplication.java
@@ -0,0 +1,13 @@
+package br.com.guru.frontendspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontendSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrontendSpringApplication.class, args);
+	}
+
+}
---
+++ frontend/frontend-spring/src/test/java/br/com/guru/frontendspring/FrontendSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.guru.frontendspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontendSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
